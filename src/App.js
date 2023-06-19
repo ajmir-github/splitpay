b307e9de2ca8f290a9bfb7fefbcb57e3809e7e0c
Ajmir Raziqi <ajmir.ng3@gmail.com>
2023-06-19 18:44:40 +0300

    styles made elegent

--- src/App.js
+++ src/App.js
@@ -16,16 +16,16 @@ function Home({ setStore }) {
     navigate("/split");
   }
   return (
-    <div className="flex flex-col gap-4 grow">
-      <div className="grow text-lg">Enter the total:</div>
+    <div className="flex flex-col gap-4 sm:gap-8 grow">
+      <div className="grow text-4xl sm:text-6xl">Enter the total:</div>
       <input
         id="totalInput"
         type="number"
-        className="input input-bordered input-primary grow text-lg"
+        className="input input-bordered input-primary grow text-xl text-center"
         value={input}
         onChange={(e) => setInput(e.target.value)}
       />
-      <button onClick={nextPage} className="btn btn-primary text-lg">
+      <button onClick={nextPage} className="btn btn-primary text-xl">
         Split
       </button>
     </div>
@@ -34,7 +34,7 @@ function Home({ setStore }) {
 
 function Indicator({ store }) {
   return (
-    <div className="stats stats-horizontal shadow">
+    <div className="stats stats-horizontal">
       <div className="stat">
         <div className="stat-title">Total</div>
         <div className="stat-value">€{store.total}</div>
@@ -79,53 +79,51 @@ function PayFor({ setStore }) {
     setSubTotal(0);
   };
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-4 sm:gap-8">
       {list.length > 0 && (
-        <div className="overflow-x-auto">
-          <table className="table">
-            {/* head */}
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>Price</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {list.map((price, index) => (
-                <tr key={index}>
-                  <th>{index + 1}</th>
-                  <td>€{price}</td>
-                  <td>
-                    <button
-                      className="btn btn-circle btn-sm btn-error"
-                      onClick={() => removeFromList(index)}
+        <table className="table table-zebra table-lg">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#Items</th>
+              <th>Prices</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {list.map((price, index) => (
+              <tr key={index}>
+                <th>{index + 1}</th>
+                <td>€{price}</td>
+                <td>
+                  <button
+                    className="btn btn-circle btn-outline btn-sm btn-error"
+                    onClick={() => removeFromList(index)}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
                     >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-6 w-6"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M6 18L18 6M6 6l12 12"
-                        />
-                      </svg>
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       )}
       <div className="join">
         <input
-          className="input input-bordered input-primary join-item grow text-lg"
+          className="input input-bordered input-primary join-item grow text-center text-lg"
           type="number"
           value={input}
           onChange={(e) => setInput(e.target.value)}
@@ -148,8 +146,8 @@ function PayFor({ setStore }) {
 
 function Split({ store, setStore }) {
   return (
-    <div className="flex flex-col gap-4 grow">
-      <Link to={"/"} className="btn grow btn-error btn-outline">
+    <div className="flex flex-col gap-4 sm:gap-8 grow">
+      <Link to={"/"} className="btn grow btn-error">
         Clear
       </Link>
       <Indicator store={store} />
@@ -169,19 +167,21 @@ function App() {
     console.log(store);
   });
   return (
-    <div className="flex justify-center p-2">
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/"
-            element={<Home store={store} setStore={setStore} />}
-          />
-          <Route
-            path="/split"
-            element={<Split store={store} setStore={setStore} />}
-          />
-        </Routes>
-      </BrowserRouter>
+    <div className="flex justify-center p-4 sm:p-8">
+      <div className="max-w-screen-sm grow">
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={<Home store={store} setStore={setStore} />}
+            />
+            <Route
+              path="/split"
+              element={<Split store={store} setStore={setStore} />}
+            />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
